929a9a879eb50e5dd2a7c9d35c39a4feeb9baade
Sazar24 <budrewicz.cezary@gmail.com>
2018-01-21 13:13:19 +0100

    one working clickable cell

--- game.js
+++ game.js
@@ -1,35 +1,34 @@
-const gameElement = document.getElementById('game');
-const row = document.createElement('tr');
-const cell1 = document.createElement('td');
-const cell2 = document.createElement('td');
+class GameCell {
+    constructor() {
+        this._state = 'unknown';        // 'podkreślnik' = info dla programisty że to private;
+        this._element = document.createElement('td');
+        const self = this;
+        this._element.addEventListener('click', () => {
+            self.setState('miss');      // na funkcji strzałkowej działa z this.
+            console.log(this);
 
-row.appendChild(cell1);
-row.appendChild(cell2);
-gameElement.appendChild(row);
+        })
+    }
 
+    setState(state) {
+        if (state !== 'unknown' && state !== 'miss' && state !== 'hit') {
+            throw new Error('Invalid state');
+        }
+        this._state = state;
+        this._element.className = "cell_" + state;
+    }
 
-const cells = [cell1, cell2];
-
-for (let i = 0; i < cells.length; i++) {
-    cells[i].addEventListener('click', function (event) {
-        event.target.classList.add('clicked');
-    });
+    getElement() {
+        return this._element;
+    }
 }
 
-// cells.forEach((elem) => {
-//     console.log(elem);
-
-//     elem.addEventListener('click', function (event) {
-//         event.target.classList.add('clicked');
-//     });
 
 
+const gameElement = document.getElementById('game');
+const row = document.createElement('tr');
+gameElement.appendChild(row);
 
+const cell1 = new GameCell;
 
-// cell1.addEventListener('click', function (event) {
-//     event.target.classList.add('clicked');
-// })
-
-// cell2.addEventListener('click', function (event) {
-//     event.target.classList.add('clicked');
-// })
+row.appendChild(cell1.getElement());
